d90d2a6b73e54c7de659173d6f64343beabe2b80
Mihail <35828413+Webring@users.noreply.github.com>
2023-06-26 00:56:33 +0700

    add task 11

---
+++ task 11/task_11.cpp
@@ -0,0 +1,98 @@
+/*
+ * Task 11:
+ * В текстовом файле даны две последовательности символов, разделенные запятой.
+ * Написать подпрограмму, которая удаляет из однонаправленного списка все вхождения заданного элемента.
+ * Используя эту подпрограмму удалить из первой последовательности все элементы, входящие во вторую последовательность.
+*/
+
+#include <iostream>
+#include "fstream"
+
+#define INPUT_FILE_PATH "../task 11/test.txt" // Имя входного файла
+
+using namespace std; //Подключаем пространство имен
+
+struct ListElement { // Структура для элемента списка
+    char value; // значение
+    ListElement *next = NULL; // указатель на следующий элемент
+};
+
+void delete_element_from_list(ListElement *first_element, char value) {
+    ListElement *iter_element = first_element;// Создаем указатель и выделяем под данные память
+    ListElement *temp_element; // указатель на удаляемый элемнт
+    while (iter_element->next != NULL) {//Пока не кончится список
+        if (iter_element->next->value == value) { //Если нашли не последнее вхождение
+            temp_element = iter_element->next; //Запоминаем элемент
+            iter_element->next = iter_element->next->next; // Меняем связи в списке
+            delete temp_element; // Удаляем элемент
+        } else {
+            iter_element = iter_element->next; // переходим на следующий элемент
+        }
+    }
+}
+
+void delete_many_elements_from_list(ListElement *first_element_of_text, ListElement *first_element_of_deleted_symbols) {
+    ListElement *iter_element = first_element_of_deleted_symbols;// Создаем указатель и выделяем под данные память
+    while (iter_element != NULL) {//Пока не кончится список
+        delete_element_from_list(first_element_of_text, iter_element->value); // Удаляем текущий символ
+        iter_element = iter_element->next; // Переходим на следующий элемент
+    }
+}
+
+void print_list(ListElement *first_element, char *title) { //Функция вывода списка на экран
+    cout << "List (" << title << ") elements:" << endl;
+    ListElement *iter_element = first_element; // Создаем указатель и выделяем под данные память
+    int total_amount = 0; // Количество элементов в списке
+    while (iter_element != NULL) { //Пока не дойдем до первого элемента
+        cout << "| " << iter_element->value << endl; // Выводим на экран элемент
+        total_amount++; // Увеличиваем число элментов на один
+        iter_element = iter_element->next; //Переходим на след элмент
+    }
+    cout << "+amount: " << total_amount << endl; //Печатаем количество
+}
+
+
+bool file_is_empty(ifstream &file) { // Проверка файла на пустоту
+    return file.peek() == ifstream::traits_type::eof(); // Если следующий доступный для чтения символ равен символу конца файла
+}
+
+int main() {
+    ifstream input_file(INPUT_FILE_PATH); // открываем файл для чтения
+
+    if (not input_file.is_open()) { // Если файл не открыт
+        cout << "Can't open input file" << endl; //Выводим сообщение об ошибке
+        return 0; // Завершаем работу программы
+    }
+
+    if (file_is_empty(input_file)) { // Проверяем файл на пустоту
+        cout << "file is empty" << endl; //Выводим сообщение об ошибке
+        return 0; // Завершаем работу программы
+    }
+
+    // Список с загланым звеном
+    ListElement *first_element_of_text, *first_element_of_deleted_symbols, *iter_element; // Создаем указатели на элементы списка: Начало списка и элемент для перебора
+    first_element_of_text = iter_element = new ListElement; // Выделяем под них память
+    first_element_of_deleted_symbols = new ListElement;
+
+    char readed_symbol; // Переменная для хранения прочтенного символа
+    while (input_file.get(readed_symbol)) { // Читаем файл посимвольно до тех пор, пока он не закончится
+
+        if (readed_symbol == ',') { // Если символ запятая
+            iter_element = first_element_of_deleted_symbols; // Переходим на другую последовательность
+        } else {
+            ListElement *new_element = new ListElement; // создаем указатель на новый элемент и  выделяем под него память
+            new_element->value = readed_symbol; // Читаем из файла новое значение и записываем ее в структуру
+            iter_element->next = new_element; // добавляем ссылку на следующий элемент
+            iter_element = iter_element->next; // Переходим на следующий элемент
+        }
+    }
+
+
+    print_list(first_element_of_text->next, "text"); // Выводим список
+    print_list(first_element_of_deleted_symbols->next, "delete symbols"); // Выводим список
+
+    delete_many_elements_from_list(first_element_of_text, first_element_of_deleted_symbols); // Удаляем символы
+
+    print_list(first_element_of_text->next, "text updated"); // Выводим список
+    return 0;
+}
